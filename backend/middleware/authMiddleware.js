298d09cd8a4e4a57f68cd68c160e7f2939af945a
Mizzaka <136617330+Mizzaka@users.noreply.github.com>
2024-06-29 11:50:41 +0530

    authMiddleware update and Creating Register

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,23 +1,20 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel')
+const User = require('../models/userModel');
 
 const protect = async (req, res, next) => {
     let token;
-    if (req.headers.authorization && req.headers.authorization.startWith('Bearer')) {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            token = req.headers.authorization.split('')[1];
+            token = req.headers.authorization.split(' ')[1]; // Use a space ' ' to split the token
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-passowrd');
+            req.user = await User.findById(decoded.id).select('-password');
             next();
         } catch (error) {
-            res.status(401).json({ error: 'Not authorized, token faild'});
-
+            res.status(401).json({ error: 'Not authorized, token failed' });
         }
-    }
-    if (!token) {
-        res.status(401).json({ error: 'Not authorized, no token'});
-
+    } else {
+        res.status(401).json({ error: 'Not authorized, no token 1' });
     }
 };
 
-module.exports = protect
+module.exports = protect;
