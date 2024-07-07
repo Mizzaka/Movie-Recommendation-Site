78afff14f4bb07d5931ed6f3019dcaad8cc212e4
Mizzaka <136617330+Mizzaka@users.noreply.github.com>
2024-07-07 13:29:15 +0530

    update

--- frontend/src/components/MediaCard.js
+++ frontend/src/components/MediaCard.js
@@ -7,7 +7,7 @@ const MediaCard = ({ id, type, image, title, releaseDate, ratings, onDelete, isI
 
   const handleWatchlistClick = async () => {
     if (!user) {
-      console.log('User must be logged in to add to watchlist');
+      alert('User must be logged in to add to watchlist');
       return;
     }
 
@@ -54,9 +54,10 @@ const MediaCard = ({ id, type, image, title, releaseDate, ratings, onDelete, isI
           onClick={isInWatchlist ? handelDeleteClick : handleWatchlistClick}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-          className={`w-6 h-6 cursor-pointer  hover:text-red-500`}
+          fill="currentColor"
+          stroke='gray'
+          strokeWidth="1"
+          className={`w-6 h-6 cursor-pointer text-transparent hover:text-primary`}
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
         </svg>
