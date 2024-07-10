d10f9fe617065878b6c08317670ceb76b24d1b1c
Mizzaka <136617330+Mizzaka@users.noreply.github.com>
2024-07-10 13:50:11 +0530

    Update categoryContoller.js

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,5 +1,6 @@
 const Movie = require('../models/movieModel');
 const Series = require('../models/seriesModel');
+const { getSignedImageUrl } = require('../utils/s3Utils');
 
 const getCategoryItems = async (req, res) => {
     const { category } = req.params;
@@ -8,7 +9,24 @@ const getCategoryItems = async (req, res) => {
         const movies = await Movie.find({ category });
         const series = await Series.find({ category });
 
-        res.status(200).json({ movies, series });
+        // GET signed URLs for movie images
+
+        const updatedMovies = await Promise.all(
+            movies.map(async (movie) => {
+                const imageUrl = await getSignedImageUrl(movie.image);
+                return { ...movie._doc, imageUrl };
+            })
+        );
+
+        // GET signed URLs for series images
+        const updatedSeries = await Promise.all(
+            series.map(async(seriesItem) => {
+                const imageUrl = await getSignedImageUrl(seriesItem.image);
+                return { ...seriesItem._doc, imageUrl };
+            })
+        );
+
+        res.status(200).json({ movies: updatedMovies, series: updatedSeries });
     } catch (error) {
         res.status(500).json( {error: 'Somthing went wrong ' });
     }
